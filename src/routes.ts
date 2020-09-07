6f64fc5b3d5f802bd4b57d3fee437b5961eab962
Gian Henrique <gian.benedito@sou.unifeob.edu.br>
2020-09-07 20:22:11 -0300

    test

--- src/routes.ts
+++ src/routes.ts
@@ -22,7 +22,7 @@ routes.delete('/books/:id', booksController.delete)
 
 routes.get('/teste', (request, response) => {
   return response.json({
-    message: 'Testando...'
+    message: 'Teste'
   })
 })
 
